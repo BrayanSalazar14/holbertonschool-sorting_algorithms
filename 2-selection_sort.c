695fa295f0d1928fcb72f33370e558fca66d2166
BrayanSalazar14 <bsalazarperdomo@gmail.com>
2024-01-29 13:29:16 -0800

    v2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -12,19 +12,20 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, aux;
+	size_t i, j, aux, min;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
+		min = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[i] > array[j])
-			{
-				aux = array[i];
-				array[i] = array[j];
-				array[j] = aux;
-				print_array(array, size);
-			}
+			if (array[j] < array[min])
+				min = j;
+
+			aux = array[i];
+			array[i] = array[min];
+			array[min] = aux;	
 		}
+		print_array(array, size);
 	}
 }
